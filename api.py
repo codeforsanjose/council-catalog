931fd3cc598bee7c2ee19f3a121bd051c0d6d9b5
Joseph <jrichardson@jmstudios.net>
2017-08-29 11:07:44 -0700

    have to run the frontend server seperately then backend api redirects to frontend for now but it is connected, now to see if can make call to backend without too much trouble

--- api.py
+++ api.py
@@ -1,11 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 
-app = Flask(__name__ , static_folder='./templates/statuc', template_folder='./templates')
+app = Flask(__name__ , static_folder='templates/static', template_folder='templates')
 
 @app.route('/')
 def index():
-    return render_template('static/index.html', name='index')
-
+    #return render_template('static/index.html', name='index')
+    return redirect('http://localhost:8080')
 
 if __name__ == '__main__':
     app.run(debug=True)
