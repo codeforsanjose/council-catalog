aa6f3488452f625d65b8f1a782a3f82b8fd1ae94
Joseph <jrichardson@jmstudios.net>
2017-08-31 18:16:56 -0700

    with keyword as transportation, was able to find 13 meetings with the keyword and return back to frontend

--- api.py
+++ api.py
@@ -13,28 +13,39 @@ def index():
     #return render_template('static/index.html', name='index')
     return redirect('http://localhost:8080')
 
+def get_posted_data_from_request():
+    data = [posted_data for posted_data in request.form.keys()]
+    return json.loads(data[0])
+
 @app.route('/api/keyword', methods=['POST', 'OPTIONS'])
 def minutes_search_route():
-    print(request.form)
-    data = [posted_data for posted_data in request.form.keys()]
-    keyword = json.loads(data[0])['keyword']
-    import pdb; pdb.set_trace()
-    #m['2017-08-29 00:00:00']['content'] will get the content for the date, thanks Matt
-    meetings_found = []
+    posted_data = get_posted_data_from_request()
+    keyword = posted_data['keyword']
+    meetings_found = navigate_through_minutes_panda(keyword)
     response_data = {
         'keyword': keyword,
         'found_meetings': meetings_found
     }
+    import pdb; pdb.set_trace()
     return make_response(json.dumps(response_data))
 
 def navigate_through_minutes_panda(keyword):
     meeting_minutes_with_keyword = []
     for timestamp in minutes_from_file.keys():
-        search_through_content(keyword, timestamp)
+        meeting_found = search_through_content(keyword, timestamp)
+        if meeting_found is not None:
+            meeting_minutes_with_keyword.append(meeting_found)
+    return meeting_minutes_with_keyword
 
 def search_through_content(keyword, timestamp_key):
     content_to_search = minutes_from_file[timestamp_key]['content']
-
+    if keyword in content_to_search:
+        return {
+            'timestamp': str(timestamp_key),
+            'meeting_content': content_to_search
+        }
+    else:
+        return None
 
 if __name__ == '__main__':
     app.run(debug=True)
