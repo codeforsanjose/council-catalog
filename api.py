d1993ede7044e02841d4641296ab58d0255d7640
Joseph <jrichardson@jmstudios.net>
2017-08-31 17:51:33 -0700

    finally got the keyword out of the posted data, i think cors put the values posted as keys in the form started searching functions to find meetings by keyword.

--- api.py
+++ api.py
@@ -4,6 +4,7 @@ import json
 from flask import Flask, render_template, redirect, request, make_response
 from flask_cors import CORS
 
+minutes_from_file = pandas.read_json('./htmlgrabber_output.json')
 app = Flask(__name__ , static_folder='templates/static', template_folder='templates')
 CORS(app)
 
@@ -15,9 +16,25 @@ def index():
 @app.route('/api/keyword', methods=['POST', 'OPTIONS'])
 def minutes_search_route():
     print(request.form)
-    minuts_from_file = pandas.read_json('./htmlgrabber_output.json')
+    data = [posted_data for posted_data in request.form.keys()]
+    keyword = json.loads(data[0])['keyword']
     import pdb; pdb.set_trace()
-    return make_response(json.dumps([]))
+    #m['2017-08-29 00:00:00']['content'] will get the content for the date, thanks Matt
+    meetings_found = []
+    response_data = {
+        'keyword': keyword,
+        'found_meetings': meetings_found
+    }
+    return make_response(json.dumps(response_data))
+
+def navigate_through_minutes_panda(keyword):
+    meeting_minutes_with_keyword = []
+    for timestamp in minutes_from_file.keys():
+        search_through_content(keyword, timestamp)
+
+def search_through_content(keyword, timestamp_key):
+    content_to_search = minutes_from_file[timestamp_key]['content']
+
 
 if __name__ == '__main__':
     app.run(debug=True)
